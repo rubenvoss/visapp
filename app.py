6b19d8d165db8e16ba6b4b2f008acfb117779e74
Ruben Voß <bekertshop@gmail.com>
2024-01-18 07:52:18 +0700

    add max size checker

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ ALLOWED_EXTENSIONS = {'heic', 'HEIC', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# The code below will limit the maximum allowed payload to 16 megabytes.
+# If a larger file is transmitted, Flask will raise a RequestEntityTooLarge exception.
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
