17e9d8d9c4318edcd16eb1751038ca6f0532fcd5
Ruben Voß <bekertshop@gmail.com>
2024-01-18 12:47:09 +0700

    working on user_data json

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import os
+import os, shortuuid
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './uploads'
+USERS_FOLDER = './users'
 ALLOWED_EXTENSIONS = {'heic', 'HEIC', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+app.config['USERS_FOLDER'] = USERS_FOLDER
 
 # The code below will limit the maximum allowed payload to 16 megabytes.
 # If a larger file is transmitted, Flask will raise a RequestEntityTooLarge exception.
@@ -20,26 +23,34 @@ def allowed_file(filename):
 def index():
     return render_template('index.html')
 
-@app.route('/application', methods=['GET', 'POST'])
+@app.route('/form', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-    return render_template('application.html')
+        unique_id = str(shortuuid.uuid())
+        user_folder = os.path.join(app.config['USERS_FOLDER'], unique_id)
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+        os.mkdir(user_folder)
 
+        face_image_filepath = os.path.join(user_folder, unique_id + '_face_image.jpg')
+        passport_image_filepath = os.path.join(user_folder, unique_id + '_passport_image.jpg')
+        face_image_filename = unique_id + '_face_image.jpg'
+        request.files['face_image'].save(os.path.join(user_folder, face_image_filename))
+        passport_image_filename = unique_id + '_passport_image.jpg'
+        request.files['passport_image'].save(os.path.join(user_folder, passport_image_filename))
+
+        user_data = {
+            'full_name': request.form['full_name'],
+            'email': request.form['email'],
+            'passport_number': request.form['passport_number'],
+            'dob': request.form['dob'],
+            'nationality': request.form['nationality'],
+            'visa_type': request.form['visa_type'],
+            'processing_time': request.form['processing_time'],
+            'port_of_arrival': request.form['port_of_arrival'],
+            'face_image_filepath': face_image_filepath,
+            'passport_image_filepath': passport_image_filepath,
+        }
+        
+        return render_template('success.html', unique_id=unique_id)
+    
+    return render_template('form.html')
