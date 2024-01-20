5baa85c7aca13eef3acdfffcb43ca74956601f90
Ruben Voß <bekertshop@gmail.com>
2024-01-20 13:11:39 +0700

    updated architecture

--- app/users.py
+++ app/users.py
@@ -2,45 +2,39 @@ import os, json
 from werkzeug.utils import secure_filename
 
 class User:
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.user_folder = self.return_user_folder(user_id=user_id)
-
-    def return_user_folder(user_id):
-        user_folder_filepath = os.path.join(app.config['USERS_FOLDER'], user_id)
-        return user_folder_filepath
-
-    def make_user_folder(user_id):
-        user_folder_filepath = return_user_folder(user_id=user_id)
+    def __init__(self, email, flask_config, form):
+        self.email = email
+        self.user_id = secure_filename(self.email.replace('@', '_').replace('.', '_'))
+        self.flask_config = flask_config
+        self.user_folder_filepath = os.path.join(self.flask_config['USERS_FOLDER'], self.user_id)
+        self.form = form
+        self.make_user_folder()
+        self.json_user_data = {}
+        self.make_json_user_data()
+        self.save_json_user_data()
+        self.save_image()
+    
+    def make_user_folder(self):
         try:
-            os.mkdir(user_folder_filepath)
+            os.mkdir(self.user_folder_filepath)
         except Exception as e:
             print(e)
-        return user_folder_filepath
 
-    def save_json_user_data(user_id, json_user_data, filename_ending='_user_data.json'):
-        json_user_data_filepath = os.path.join(return_user_folder(user_id=user_id), user_id + filename_ending)
-        with open(json_user_data_filepath, 'w', encoding='utf-8') as file:
-            json.dump(json_user_data, file, indent=4, sort_keys=True, default=str)
-        return json_user_data_filepath
-
-    def secure_user_email(email):
-        email = email.replace('@', '_')
-        email = email.replace('.', '_')
-        return secure_filename(email)
-
-    def make_json_data(form):
+    def make_json_user_data(self):
         json_data = {}
-        for field in form:
+        for field in self.form:
             json_data[field.name] = field.data
+        self.json_user_data = json_data
         return json_data
 
-    def save_image(user_id):
-        pass
-            # user_folder = os.path.join(app.config['USERS_FOLDER'], unique_id)
+    def save_json_user_data(self, filename_ending='_user_data.json'):
+        json_user_data_filepath = os.path.join(self.user_folder_filepath, self.user_id + filename_ending)
+        with open(json_user_data_filepath, 'w', encoding='utf-8') as file:
+            json.dump(self.json_user_data, file, indent=4, sort_keys=True, default=str)
+        return json_user_data_filepath
 
-
-            # face_image_filepath = os.path.join(user_folder, unique_id + '_face_image.jpg')
+    def save_image(self, filename_ending='_image.jpg'):
+            face_image_filepath = os.path.join(self.user_folder_filepath, self.user_id + filename_ending)
             # passport_image_filepath = os.path.join(user_folder, unique_id + '_passport_image.jpg')
             
             # face_image_filename = unique_id + '_face_image.jpg'
