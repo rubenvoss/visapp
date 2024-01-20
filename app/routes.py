125b8d4d1d5a7b73127a443a617adbed85b87b25
Ruben Voß <105738004+rubenvoss@users.noreply.github.com>
2024-01-20 10:23:03 +0100

    15 convert image to jpeg + under 1mb (#21)
    - image is now converted and saved to disk as jpeg

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from app.forms import ApplicationForm
+from app.forms import VisaApplicationForm
 from app.users import User
 
 @app.route('/', methods=['GET'])
@@ -10,8 +10,8 @@ def index():
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    form = ApplicationForm()
+    form = VisaApplicationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, flask_config=app.config, form=form)
+        user = User(flask_config=app.config, form=form)
         return render_template('success.html', email=user.email)
     return render_template('form.html', form=form)
