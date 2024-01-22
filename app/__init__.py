e3cf4f432fca8331b032e0ee81387d09980f7558
Ruben Voß <bekertshop@gmail.com>
2024-01-22 07:24:21 +0700

    change structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-"""Init file for the app package."""
+"""To start the application."""
 from flask import Flask
-from config import Config
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
