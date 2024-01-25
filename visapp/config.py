3140b09d54c23992c615f3a2b14bfe5cdcc26bb5
Ruben Voß <bekertshop@gmail.com>
2024-01-25 10:58:59 +0700

    add config

--- visapp/config.py
+++ visapp/config.py
@@ -6,3 +6,6 @@ class Config:
     SESSION_TYPE = os.environ.get('SESSION_TYPE') or 'filesystem'
     USERS_FOLDER = os.environ.get('USERS_FOLDER') or './users'
     MAX_CONTENT_LENGTH = os.environ.get('MAX_CONTENT_LENGTH') or 16 * 1000 * 1000
+    AUTH0_CLIENT_ID = os.environ.get('AUTH0_CLIENT_ID') or 'aK8UaoCYUFk9k1g6dsKIFcJ5BsBaVPze'
+    AUTH0_CLIENT_SECRET = os.environ.get('AUTH0_CLIENT_SECRET') or 'mLrYWQgEg_ZAAkpAm55ds_1mHlxufu4aEKWrskRIdhPbkSdAQ7IA2A1pJdQiw7Zf'
+    AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN') or 'dev-3dqlnzckjzka4668.us.auth0.com'
