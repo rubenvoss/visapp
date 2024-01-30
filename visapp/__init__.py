fe5c39920987e5fcc3d8eaec7de125c6d9694618
Ruben Voß <bekertshop@gmail.com>
2024-01-30 09:50:04 +0700

    form blueprint

--- visapp/__init__.py
+++ visapp/__init__.py
@@ -1,14 +1,15 @@
 """To start the application."""
 from flask import Flask
 from visapp.config import Config
-from visapp import auth
-import os
+from visapp import auth, forms
 from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 oauth = OAuth(app)
+
 auth.init_app(oauth)
 
 app.register_blueprint(auth.bp)
+app.register_blueprint(forms.bp)
--- visapp/forms.py
+++ visapp/forms.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, DateField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Length
+from flask import render_template, Blueprint
 
 class VisaApplicationForm(FlaskForm):
     full_name = StringField('Full Name:', validators=[DataRequired(), Length(min=3, max=100)])
@@ -12,3 +13,12 @@ class VisaApplicationForm(FlaskForm):
     face_image = FileField('''Clear Image of your face, no Glasses, look straight
                            into Camera''', validators=[FileRequired(['jpg','jpeg','png'])])
     submit = SubmitField('Submit')
+
+bp = Blueprint('forms', __name__)
+
+@bp.route("/form", methods=["GET", "POST"])
+def form():
+    visa_form = VisaApplicationForm()
+    if visa_form.validate_on_submit():
+        return render_template('success.html', indent=4)
+    return render_template('form.html', form=visa_form)
