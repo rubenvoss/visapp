64ea22186b6de6c8066711c3708b8c83fa74d30d
Ruben Voß <bekertshop@gmail.com>
2024-03-16 18:44:59 +0100

    init django visapp

---
+++ visapp/visapp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for visapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visapp.settings")
+
+application = get_asgi_application()
